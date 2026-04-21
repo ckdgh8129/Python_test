e3a6f6364ee115a93b8b45e76e08563aa3a42471
ckdgh8129 <ckdgh8129@gmail.com>
2026-04-21 12:20:57 +0900

    123

---
+++ test.py
@@ -0,0 +1,31 @@
+print("와우")
+num=10
+num2=30
+result = num+num2
+print(result)
+name ="이순신"
+print(name)
+job="군인"
+print(name+job+"이다")
+print("배고파" *5)
+반지름=5
+pi = 3.14
+print(반지름 *2*pi)
+is_student = True
+is_food = False
+print(is_student)
+print(type(pi))
+num3= input("정수입력 : ")
+
+num3=int(num3)
+print(num3 + 10)
+#한줄주석 이다
+"""
+여러줄
+주석이다
+"""
+#문제, 너비와 높이를 입력받아 사각형의 넓이를 출력하세요
+너비= int(input("너비 : "))
+높이= int(input("높이 : "))
+넓이=너비*높이
+print(넓이)
